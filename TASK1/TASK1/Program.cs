18dfa1b837b2b6ba2ab794943db268246c57c6d7
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-12 02:12:22 +0400

    Task3

--- TASK1/TASK1/Program.cs
+++ TASK1/TASK1/Program.cs
@@ -8,12 +8,24 @@
 //    Console.WriteLine("Eded tek ededdir");
 //}
 
-string Username = "Admin", Password = "Admin123";
-if (Username == "Admin" && Password == "Admin123")
+//string Username = "Admin", Password = "Admin123";
+//if (Username == "Admin" && Password == "Admin123")
+//{
+//    Console.WriteLine("Xosh gelmisiniz");
+//}
+//else
+//{
+//    Console.WriteLine("Username ve ya password yanlisdir!");
+//}
+
+int num = 124, num1 = 8;
+if (num % 2 == 0 && num1 % 2 == 0)
 {
-    Console.WriteLine("Xosh gelmisiniz");
+    Console.WriteLine(num + num1);
 }
 else
 {
-    Console.WriteLine("Username ve ya password yanlisdir!");
+    Console.WriteLine("daxil olunan ededler cut olmalidir!");
 }
+
+

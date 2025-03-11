65b5a50321e0a13b99df0576a24eebd35978fc76
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-12 02:16:23 +0400

    Task4

--- TASK1/TASK1/Program.cs
+++ TASK1/TASK1/Program.cs
@@ -18,14 +18,39 @@
 //    Console.WriteLine("Username ve ya password yanlisdir!");
 //}
 
-int num = 124, num1 = 8;
-if (num % 2 == 0 && num1 % 2 == 0)
+//int num = 124, num1 = 8;
+//if (num % 2 == 0 && num1 % 2 == 0)
+//{
+//    Console.WriteLine(num + num1);
+//}
+//else
+//{
+//    Console.WriteLine("daxil olunan ededler cut olmalidir!");
+//}
+
+int num = 76;
+if (num >= 91 && num <= 100)
 {
-    Console.WriteLine(num + num1);
+    Console.WriteLine("A");
+}
+else if (num >= 81 && num <= 90)
+{
+    Console.WriteLine("B");
+}
+else if (num >= 71 && num <= 80)
+{
+    Console.WriteLine("C");
+}
+else if (num >= 61 && num <= 70)
+{
+    Console.WriteLine("D");
+}
+else if (num >= 51 && num <= 60)
+{
+    Console.WriteLine("E");
 }
 else
 {
-    Console.WriteLine("daxil olunan ededler cut olmalidir!");
+    Console.WriteLine("Terbrikler kesilmisiniz!");
 }
-
 

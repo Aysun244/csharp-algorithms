baa4b0394b31d53a9cdadbdeb3ec0b59ee15ba97
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-14 00:31:42 +0400

    Task10

--- TASK1/TASK1/Program.cs
+++ TASK1/TASK1/Program.cs
@@ -106,18 +106,18 @@
 //    Console.WriteLine("Cem:" + sum);
 //}
 
-int num = 1243;
-int max = 0;
-int a;
-while(num>0)
-{
-    a = num % 10;
-        if(a>max)
-    {
-        max = a;
-    }
-    num /= 10;
-}
-{
-    Console.WriteLine(max);
-}
+//int num = 1243;
+//int max = 0;
+//int a;
+//while(num>0)
+//{
+//    a = num % 10;
+//        if(a>max)
+//    {
+//        max = a;
+//    }
+//    num /= 10;
+//}
+//{
+//    Console.WriteLine(max);
+//}
